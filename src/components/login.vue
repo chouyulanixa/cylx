0ee127879bdd2bedf3544f5eea86c59f14dc891f
jianning556 <592230003@qq.com>
2018-12-04 00:31:25 +0800

    login.vue

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,7 +1,18 @@
+<script src="../main.js"></script>
 <template>
-  <div>
-   <h2>login</h2>
-   <router-link to="/login"></router-link>
+  <div id="app">
+    <h1 class="top">臭鱼烂虾社区</h1>
+    <div class="from">
+      <from>
+        <input v-model="message" placeholder=" 账号" class="from-1">
+        <input v-model="message" placeholder=" 密码"class="from-1">
+        <input type="submit" value="提交" class="from-2">
+      </from>
+       <p class="zhuce">
+         <span>注册账号</span>
+         <span>忘记密码</span>
+       </p>
+    </div>
   </div>
 </template>
 
@@ -15,3 +26,47 @@ export default {
   }
 }
 </script>
+<style>
+#app{
+  width: 100%;
+  margin: 0 auto;
+}
+  .from{
+    width: 500px;
+    margin: 0 auto;
+    margin-top: 10%;
+  }
+  .from-1{
+    width: 100%;
+    height: 60px;
+    font-size: 24px;
+    border: 1px solid #aaa;
+    border-radius: 10px;
+    outline: none;
+    margin: 0 auto;
+    margin-top: 10px;
+  }
+.from-2{
+  width: 100%;
+  height: 60px;
+  font-size: 24px;
+  margin-top: 10px;
+  border: none;
+  border-radius: 10px;
+  outline: none;
+  background-color: #e4b9c0;
+  color: #fff;
+  cursor: pointer;
+}
+  .zhuce span{
+    cursor: pointer;
+    margin-right: 20px;
+  }
+.zhuce span:hover{
+  color: red;
+}
+  .top{
+    text-align: center;
+    margin-top: 8%;
+  }
+</style>
---
+++ src/components/register.vue
@@ -0,0 +1,12 @@
+<template>
+
+  <p class="zhuce">
+    <span>注册账号</span>
+    <span>忘记密码</span>
+  </p>
+</template>
+
+
+<script>
+
+</script>
