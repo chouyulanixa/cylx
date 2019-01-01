f820616f8f23cd182177fde3fedd5f2dec34e7b6
wenchengshen <634284013@qq.com>
2019-01-01 17:51:59 +0800

    轮播图

--- src/components/banner.vue
+++ src/components/banner.vue
@@ -1,56 +1,56 @@
 <template>
      <div id="banner">
-         <slider :pages="pageslist" :sliderinit="sliderinits">
-            <!-- <slot>12312</slot>     -->
-        </slider>
+         <swiper :options="swiperOption" ref="mySwiperA" >
+              <!-- 幻灯内容 -->
+              <swiper-slide style="float:left;"><img style="max-width:100%;" src="../img/xia.png" alt=""></swiper-slide>
+              <swiper-slide style="float:left;"><img style="max-width:100%;" src="../img/ya.jpg" alt=""></swiper-slide>
+              <swiper-slide style="float:left;"><img style="max-width:100%;" src="../img/shu.jpg" alt=""></swiper-slide>
+              <swiper-slide style="float:left;"><img style="max-width:100%;" src="../img/niubi.jpg" alt=""></swiper-slide>
+              <swiper-slide style="float:left;"><img style="max-width:100%;" src="../img/xinxin.jpg" alt=""></swiper-slide>
+              <!-- 以下控件元素均为可选 -->
+              <div class="swiper-pagination" slot="pagination"></div>
+              <div class="swiper-button-prev" slot="button-prev"></div>
+              <div class="swiper-button-next" slot="button-next"></div>
+              <div class="swiper-scrollbar"  slot="scrollbar"></div>
+          </swiper>
      </div>
 </template>
 
 <script>
-import  slider from 'vue-concise-slider'
+import VueAwesomeSwiper from 'vue-awesome-swiper';
+import Vue from 'vue'
+Vue.use(VueAwesomeSwiper)
 export default {
   name:'banner',
   data(){
     return{
-          //图片列表[arr]
-        pageslist:[
-          {
-            title: '1123123123',
-            style:{
-                width:300,
-                background:'url(../img/xia.png)'
-            }
-          },
-          {
-           title: '21231231231231',
-           style:{
-               width:300,
-               background:'url(../img/xia.png)'
-            }
-          },
-          {
-            title: 'slide3',
-            style:{
-              background:'#4bbfc3',
-            },
+        swiperOption: {
+          // 所有配置均为可选（同Swiper配置） 
+          notNextTick: true,
+          autoplay: true,
+          grabCursor : true,
+          setWrapperSize :true,
+          autoHeight: true,
+          pagination : '.swiper-pagination',
+          paginationClickable :true,
+          prevButton:'.swiper-button-prev',
+          nextButton:'.swiper-button-next',
+          scrollbar:'.swiper-scrollbar',
+          mousewheelControl : true,
+          observeParents:true,
+          onTransitionStart(swiper){
+           console.log(swiper)
           }
-        ],
-          //滑动配置[obj]
-        sliderinits: {
-          currentPage: 1,//当前页码ssss
-          thresholdDistance: 300,//滑动判定距离
-          thresholdTime: 100,//滑动判定时间
-          autoplay:1000,//自动滚动[ms]
-          loop:true,//循环滚动
-        //   direction:'horizontal',//方向设置，垂直滚动
-          infinite:0,//无限滚动前后遍历数
-          slidesToScroll:1,//每次滑动项数
         }
     }
   },
-   components: {
-        slider
-    }
+  swiper() {
+       return this.$refs.mySwiperA.swiper
+  },
+   mounted() {
+      console.log("每次切换都会触发我");
+      this.swiper.slideTo(3, 1000, false)
+   }
 }
 </script>
 
@@ -58,6 +58,7 @@ export default {
    #banner{
        width:100%;
        height:300px;
+       overflow: hidden;
    }  
 </style>
 
